1bf4dd38f289d6e22bdac9f6def4550b608c9c8c
Jaxcoder <codenamejason@outlook.com>
2023-06-24 22:27:07 -0400

    some updates and cleanup

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,4 @@
-import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
+import Container from "./container";
 
 const Footer = () => {
   return (
@@ -7,7 +6,7 @@ const Footer = () => {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            {"<jaxcoder />"}
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
@@ -17,7 +16,7 @@ const Footer = () => {
               Read Documentation
             </a>
             <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+              href={`https://github.com/codenamejason/blog.jaxcoder.xyz`}
               className="mx-3 font-bold hover:underline"
             >
               View on GitHub
@@ -26,7 +25,7 @@ const Footer = () => {
         </div>
       </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
