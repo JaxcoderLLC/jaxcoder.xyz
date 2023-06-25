90429ed287430cc0e79fcd09ab399a4fcf03e321
Jaxcoder <codenamejason@outlook.com>
2023-06-25 17:35:12 -0400

    add google tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-EG6JW42WH8"
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-EG6JW42WH8');
+              `,
+        }}
+      />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
