ad045bae8d8be5aa51010dcd87bd5b98ea677426
Jaxcoder <codenamejason@outlook.com>
2024-01-20 15:15:32 -0500

    update home page/add dashboard layout

---
+++ src/app/members/page.tsx
@@ -0,0 +1,9 @@
+import Dashboard from "@/components/Dashboard";
+
+export default function Members() {
+    return (
+      <div>
+        <Dashboard />
+      </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between pt-80">
+      {/* Hidden button / show on hover - onClick() go to member mint page. Enjoy 🥳 */}
+      <div>
+        <Link
+          href="/members"
+        >
+          <div className="relative inline-flex items-center justify-center">
+            <div className="w-16 h-16 hover:bg-blue-500 rounded-full"></div>
+            <div className="w-16 h-16 hover:bg-blue-500 rounded-full absolute top-0 left-0 animate-ping"></div>
+            <div className="w-16 h-16 hover:bg-blue-500 rounded-full absolute top-0 left-0 animate-pulse"></div>
+          </div>
+        </Link>
+      </div>
+    </main>
   );
 }
